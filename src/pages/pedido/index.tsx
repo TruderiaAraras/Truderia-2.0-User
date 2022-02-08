36da951ba5a1d0ba70f6be0bc93a87f77aebef55
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2022-02-07 22:51:13 -0300

    Começando com Styled Components

--- src/pages/pedido/index.tsx
+++ src/pages/pedido/index.tsx
@@ -1,38 +1,33 @@
 import React from "react";
-import { ContainerPedido, Forms } from "./style";
+import { ContainerPedido, Card, CardTitle, Button } from "./style";
 import { Formik, Field, Form } from "formik";
 
 export const PagePedido: React.FC = () => {
   return (
     <ContainerPedido>
-      <Formik
-        initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
-        }}
-        onSubmit={async (values) => {
-          await new Promise((r) => setTimeout(r, 500));
-          alert(JSON.stringify(values, null, 2));
-        }}
-      >
-        <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field id="firstName" name="firstName" placeholder="Isaque" />
-
-          <label htmlFor="lastName">Last Name</label>
-          <Field id="lastName" name="lastName" placeholder="Schuwarte" />
-
-          <label htmlFor="email">Email</label>
-          <Field
-            id="email"
-            name="email"
-            placeholder="exemplo@teste.com"
-            type="email"
-          />
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
+      <Card>
+        <CardTitle>Título</CardTitle>
+        <Formik
+          initialValues={{
+            nome: "",
+            celular: ""
+          }}
+          onSubmit={async (values) => {
+            await new Promise((r) => setTimeout(r, 500));
+            alert(JSON.stringify(values, null, 2));
+          }}
+        >
+          <Form>
+            <label htmlFor="nome">Nome: </label>
+            <Field id="nome" name="nome" placeholder="Isaque Schuwarte" />
+            <hr />
+            <label htmlFor="celular">Celular: </label>
+            <Field id="celular" name="celular" placeholder="(99) 99999-9999" />
+            <hr />
+            <Button type="submit">Submit</Button>
+          </Form>
+        </Formik>
+      </Card>
     </ContainerPedido>
   );
 };
--- src/pages/pedido/style.ts
+++ src/pages/pedido/style.ts
@@ -1,9 +1,36 @@
 import styled from 'styled-components';
 
 export const ContainerPedido = styled.div`
-  
+    display: flex;
+    overflow: hidden;
+    padding: 30px;
 `;
 
-export const Forms = styled.form`
+export const Card = styled.div`
+    > form > input {
+        border: gray solid 1px;
+        border-radius: 3px;
+        padding: 4px;
+    }
+    display-flex: column;
+    border: brown solid 1px;
+    border-radius: 3px;
+    padding: 8px;
+`;
 
+export const CardTitle = styled.div`
+    background-color: red;
+    text-align: center;
+    margin-bottom: 15px;
+`
+
+export const Button = styled.button`
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    font-size: 16px;
+    margin-top: 15px;
 `
