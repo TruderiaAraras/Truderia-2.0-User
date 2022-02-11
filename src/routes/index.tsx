d768c9e02022b3202134e76782d4eb35331cc1c0
Joao Victor <joaovictordeveloper@outlook.com>
2022-02-11 00:30:17 -0300

    ajustando route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,13 +5,11 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
-import { Page1, Page2, Home } from '../pages';
+import { Home } from '../pages';
 
 const Router = (): React.ReactElement => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Page1 />} />
-      <Route path="/Page2" element={<Page2 />} />
       <Route path="/Home" element={<Home />} />
     </Routes>
   </BrowserRouter>
