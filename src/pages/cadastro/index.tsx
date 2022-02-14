a369620402e79d0870c9f3c937366c71ba6575a2
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2022-02-14 18:54:10 -0300

    Aprendendo

--- src/pages/cadastro/index.tsx
+++ src/pages/cadastro/index.tsx
@@ -1,64 +1,31 @@
 import React from "react";
-import { Formik } from "formik";
+import { Formik, Form, Field } from "formik";
 
 export const Cadastro: React.FC = () => {
-  
   return (
     <div>
-     <h1>Anywhere in your app!</h1>
-     <Formik
-       initialValues={{ email: '', password: '' }}
-       validate={values => {
-         const errors = { email: "" };
-         if (!values.email) {
-           errors.email = 'Required';
-         } else if (
-           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-         ) {
-           errors.email = 'Invalid email address';
-         }
-         return errors;
-       }}
-       onSubmit={(values, { setSubmitting }) => {
-         setTimeout(() => {
-           alert(JSON.stringify(values, null, 2));
-           setSubmitting(false);
-         }, 400);
-       }}
-     >
-       {({
-         values,
-         errors,
-         touched,
-         handleChange,
-         handleBlur,
-         handleSubmit,
-         isSubmitting,
-         /* and other goodies */
-       }) => (
-         <form onSubmit={handleSubmit}>
-           <input
-             type="email"
-             name="email"
-             onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.email}
-           />
-           {errors.email && touched.email && errors.email}
-           <input
-             type="password"
-             name="password"
-             onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.password}
-           />
-           {errors.password && touched.password && errors.password}
-           <button type="submit" disabled={isSubmitting}>
-             Submit
-           </button>
-         </form>
-       )}
-     </Formik>
-   </div>
+      <h1>Anywhere in your app!</h1>
+      <Formik
+        initialValues={{
+          name: "",
+          age: ""
+        }}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form>
+          <div>
+            <label htmlFor="name">Nome: </label>
+            <Field id="name" name="name" type="text" style={{ border: '2px ridge gray', borderRadius: '5px', padding: '4px' }} />
+          </div>
+          <div>
+            <label htmlFor="age">Idade: </label>
+            <Field id="age" name="age" type="number" style={{ border: '2px ridge gray', borderRadius: '5px', padding: '4px' }} />
+          </div>
+        </Form>
+      </Formik>
+    </div>
   );
 };
