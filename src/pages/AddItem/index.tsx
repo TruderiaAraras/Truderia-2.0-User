f926510683ded09a51a1b060de86a798c9d1a636
Joao Victor <joaovictordeveloper@outlook.com>
2022-02-15 13:47:07 -0300

    tela de addItem

--- src/pages/AddItem/index.tsx
+++ src/pages/AddItem/index.tsx
@@ -1,11 +1,106 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import { Container } from './style';
+import { InputOptions } from "../../components/InputOptions";
+
+import {
+  Container,
+  ButtonClose,
+  Header,
+  Photo,
+  InfoWrapper,
+  Title,
+  Description,
+  OptionInput,
+  TitleItem,
+  WrapperOption,
+  TextArea,
+  Footer,
+  ButtonQuantity,
+  IconAdd,
+  IconSub,
+  ButtonAdd,
+} from "./style";
 
 export const AddItem: React.FC = () => {
+  const [totalCount, setTotalCount] = useState(0);
+  const [quantityProduct, setQuantityProduct] = useState(0);
+
+  function handleClose() {
+    console.log("teste");
+  }
+
+  function handleCount(value: string) {
+    if (value === "+") {
+      setQuantityProduct(state => state + 1);
+    } else {
+      if (quantityProduct <= 0) return;
+      setQuantityProduct(state => state - 1);
+    }
+  }
+
+  function transformToRealBRL(value: number) {
+    return new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(value);
+  }
+
   return (
     <Container>
-      <p>teste</p>
+      <Header>
+        <ButtonClose onClick={handleClose}>
+          <span>Cancelar</span>
+        </ButtonClose>
+        <Photo src="http://github.com/joaovictormartin.png" />
+      </Header>
+
+      <InfoWrapper>
+        <Title>ESPECIAL DE NATAL TRUDELTTONE</Title>
+        <Description>
+          Nossa deliciosa e exclusiva massa romena, com um recheio muito
+          especial de Natal: brigadeiro branco sabor panetone e frutas
+          cristalizadas!
+        </Description>
+      </InfoWrapper>
+
+      <OptionInput>
+        <TitleItem>Sorvetes</TitleItem>
+        <WrapperOption>
+          {[1,2,3,4].map(item => (
+            <InputOptions key={item} totalCount={totalCount} setTotalCount={setTotalCount}/>
+          ))}
+        </WrapperOption>
+      </OptionInput>
+
+      <OptionInput>
+        <TitleItem>Bebidas</TitleItem>
+        <WrapperOption>
+          {[1,2,3,].map(item => (
+            <InputOptions key={item} totalCount={totalCount} setTotalCount={setTotalCount}/>
+          ))}
+        </WrapperOption>
+      </OptionInput>
+
+      <OptionInput>
+        <TitleItem>OBSERVAÇÕES</TitleItem>
+        <TextArea />
+      </OptionInput>
+
+      <Footer>
+        <ButtonQuantity>
+          <span>Qntde.</span>
+          <div>
+            <IconSub onClick={() => handleCount("-")} />
+            <span>{quantityProduct}</span>
+            <IconAdd onClick={() => handleCount("+")} />
+          </div>
+        </ButtonQuantity>
+
+        <ButtonAdd>
+            <span>Adicionar ao carrinho</span>
+            <span>{transformToRealBRL(100)}</span>
+        </ButtonAdd>
+      </Footer>
     </Container>
   );
 };
--- src/pages/AddItem/style.ts
+++ src/pages/AddItem/style.ts
@@ -1,5 +1,165 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { FaPlus } from "react-icons/fa"; //Icon de add
+import { FaMinus } from "react-icons/fa"; // Icon de sub
 
-export const Container = styled.div`
+export const Container = styled.div``;
+
+export const Header = styled.div`
+  width: 100%;
+`;
+
+export const ButtonClose = styled.div`
+  > span {
+    cursor: pointer;
+    position: fixed;
+    background: ${({ theme }) => theme.color.ALERT};
+    font-weight: bold;
+    font-size: 16px;
+    color: ${({ theme }) => theme.color.SHAPE};
+    padding: 5px 8px;
+    border-radius: 7px;
+    left: 5px;
+    top: 5px;
+    border: 1px solid transparent;
+  }
+`;
+
+export const Photo = styled.img`
+  max-height: 40vh;
+  width: 100%;
+  object-fit: cover;
+`;
+
+export const InfoWrapper = styled.div`
+  max-width: 1000px;
+  width: 90%;
+  margin: auto auto;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Title = styled.h1`
+  color: ${({ theme }) => theme.color.BLACK};
+  font-family: "Roboto Condensed", sans-serif;
+  font-size: 16px;
+  text-transform: uppercase;
+  padding: 16px 8px;
+`;
+
+export const Description = styled.p`
+  color: ${({ theme }) => theme.color.BLACK};
+  font-family: "Roboto Condensed", sans-serif;
+  font-size: 16px;
+  padding: 16px 8px;
+`;
+
+export const OptionInput = styled.div``;
+
+export const TitleItem = styled.p`
+  color: ${({ theme }) => theme.color.SHAPE};
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+  text-transform: uppercase;
+  background-color: ${({ theme }) => theme.color.PRIMARY};
+`;
+
+export const WrapperOption = styled.div`
+  color: ${({ theme }) => theme.color.BLACK};
+  display: flex;
+  flex-direction: column;
+  margin: 16px auto;
+`;
+
+export const Footer = styled.div`
+  margin-top: auto;
+  margin-bottom: 16px;
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+`;
+
+export const ButtonQuantity = styled.button`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 16px 24px;
+  border-radius: 4px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  background-color: ${({ theme }) => theme.color.PRIMARY};
+
+  span {
+    width: 100%;
+    font-weight: bold;
+    font-size: 16px;
+    font-family: "Roboto Condensed", sans-serif;
+    text-align: flex-end;
+    color: ${({ theme }) => theme.color.SHAPE};
+  }
+
+  div {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-top: 6px;
+    color: ${({ theme }) => theme.color.SHAPE};
+
+    span {
+      padding: 0 10px;
+    }
+  }
+`;
+
+export const IconAdd = styled(FaPlus)`
+  width: 16px;
+  height: 16px;
+  color: ${({ theme }) => theme.color.SHAPE};
+`;
+
+export const IconSub = styled(FaMinus)`
+  width: 16px;
+  height: 16px;
+  color: ${({ theme }) => theme.color.SHAPE};
+`;
+
+export const ButtonAdd = styled.button`
+  width: 60%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+  background: ${({ theme }) => theme.color.SUCCESS};
+  box-shadow: 2px 2px 12px -4px rgb(0 0 0 / 60%);
+  font-weight: bold;
+  cursor: pointer;
+
+  padding: 16px 24px;
+  border-radius: 4px;
+
+  span {
+    color: white;
+    font-family: "Roboto Condensed", sans-serif;
+    font-size: 16px;
+  }
+`;
+
+export const TextArea = styled.textarea`
+  width: calc(100% - 12px);
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  color: black;
+  height: 150px;
+  margin: 8px 6px;
+  border-radius: 4px;
+  padding: 6px;
+
+  font-size: 15px;
+  font-family: "Roboto Condensed", sans-serif;
   
+  &:focus {
+    border: 1px solid ${({theme}) => theme.color.PRIMARY};
+  }
 `;
