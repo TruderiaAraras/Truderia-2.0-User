2c37962bff0b9af4aa749cacec4fbf2251ce8a53
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2022-02-15 23:24:36 -0300

    Esperando Mais informações para prosseguir

--- src/pages/cadastro/index.tsx
+++ src/pages/cadastro/index.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import * as yup from "yup";
 import { pt } from "yup-locale-pt";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { Container } from "./style";
+import {
+  Container,
+  FormControl,
+  SubmitButton,
+  Title,
+  SharedControl,
+} from "./style";
 
 export const Cadastro: React.FC = () => {
-
-  yup.setLocale(pt)
+  yup.setLocale(pt);
   const validation = yup.object().shape({
     nome: yup.string().required(),
     email: yup.string().email().required(),
@@ -18,87 +23,100 @@ export const Cadastro: React.FC = () => {
     bairro: yup.string().required(),
     cidade: yup.string().required(),
     estado: yup.string().required(),
-    complemento: yup.string().notRequired()
+    complemento: yup.string().notRequired(),
   });
 
   return (
-    <Container>
-      <h1>FORMULÁRIO DE CADASTRO</h1>
-      <Formik
-        initialValues={{
-          nome: "",
-          email: "",
-          aniversario: "",
-          celular: "",
-          cep: "",
-          rua: "",
-          numero: "",
-          bairro: "",
-          cidade: "",
-          estado: "",
-          complemento: ""
-        }}
-        onSubmit={async (values) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-          }, 500);
-        }}
-        validationSchema={validation}
-      >
-        <Form> 
-          <div>
-            <label htmlFor="nome">Nome: </label>
-            <Field id="nome" name="nome" type="text" />
-            <ErrorMessage component="span" name="nome" />
-          </div>
-          <div>
-            <label htmlFor="email">E-mail: </label>
-            <Field id="email" name="email" type="email" />
-            <ErrorMessage component="span" name="email" />
-          </div>
-          <div>
-            <label htmlFor="aniversario">Aniversário: </label>
-            <Field id="aniversario" name="aniversario" type="date" />
-            <ErrorMessage component="span" name="aniversario" />
-          </div>
-          <div>
-            <label htmlFor="celular">Celular: </label>
-            <Field id="celular" name="celular" type="text" />
-            <ErrorMessage component="span" name="celular" />
-          </div>
-          <div>
-            <label htmlFor="cep">CEP: </label>
-            <Field id="cep" name="cep" type="text" />
-            <ErrorMessage component="span" name="cep" /> 
-          </div>
-          <div>
-            <label htmlFor="rua">Rua: </label>
-            <Field id="rua" name="rua" type="text" />
-            <ErrorMessage component="span" name="rua" />
-            <hr />
-            <label htmlFor="numero">Número: </label>
-            <Field id="numero" name="numero" type="text" />
-            <ErrorMessage component="span" name="numero" />
-            <hr />
-            <label htmlFor="bairro">Bairro: </label>
-            <Field id="bairro" name="bairro" type="text" />
-            <ErrorMessage  component="span" name="bairro" />
-            <hr />
-            <label htmlFor="cidade">Cidade: </label>
-            <Field id="cidade" name="cidade" type="text" />
-            <ErrorMessage component="span" name="cidade" />
-            <hr />
-            <label htmlFor="estado">Estado: </label>
-            <Field id="estado" name="estado" type="text" />
-            <ErrorMessage component="span" name="estado" />
-            <hr />
-            <label htmlFor="complemento">Complemento: </label>
-            <Field id="complemento" name="complemento" type="text" />
-            <ErrorMessage component="span" name="complemento" />
-          </div>
-          <button type="submit">ENVIAR</button>
-        </Form>
-      </Formik>
-    </Container>
+    <div>
+      <Title>FORMULÁRIO DE CADASTRO</Title>
+      <Container>
+        <Formik
+          initialValues={{
+            nome: "",
+            email: "",
+            aniversario: "",
+            celular: "",
+            cep: "",
+            rua: "",
+            numero: "",
+            bairro: "",
+            cidade: "",
+            estado: "",
+            complemento: "",
+          }}
+          onSubmit={async (values) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+            }, 500);
+          }}
+          validationSchema={validation}
+        >
+          <Form>
+            <SharedControl>
+              <div className="sharedNome">
+                <label htmlFor="nome">Nome: </label>
+                <hr />
+                <Field id="nome" name="nome" type="text" />
+                <ErrorMessage component="span" name="nome" />
+              </div>
+              <div className="sharedAniversario">
+                <label htmlFor="aniversario">Aniversário: </label>
+                <hr />
+                <Field id="aniversario" name="aniversario" type="date" />
+                <ErrorMessage component="span" name="aniversario" />
+              </div>
+            </SharedControl>
+            <FormControl>
+              <label htmlFor="email">E-mail: </label>
+              <Field id="email" name="email" type="email" />
+              <ErrorMessage component="span" name="email" />
+            </FormControl>
+            <FormControl>
+              <label htmlFor="celular">Celular: </label>
+              <Field id="celular" name="celular" type="text" />
+              <ErrorMessage component="span" name="celular" />
+            </FormControl>
+            <FormControl>
+              <label htmlFor="cep">CEP: </label>
+              <Field id="cep" name="cep" type="text" />
+              <ErrorMessage component="span" name="cep" />
+            </FormControl>
+            <FormControl>
+              <label htmlFor="rua">Rua: </label>
+              <Field id="rua" name="rua" type="text" />
+              <ErrorMessage component="span" name="rua" />
+              <hr />
+              <label htmlFor="numero">Número: </label>
+              <Field id="numero" name="numero" type="text" />
+              <ErrorMessage component="span" name="numero" />
+              <hr />
+              <label htmlFor="bairro">Bairro: </label>
+              <Field id="bairro" name="bairro" type="text" />
+              <ErrorMessage component="span" name="bairro" />
+              <hr />
+              <label htmlFor="cidade">Cidade: </label>
+              <Field id="cidade" name="cidade" type="text" />
+              <ErrorMessage component="span" name="cidade" />
+              <hr />
+              <label htmlFor="estado">Estado: </label>
+              <Field id="estado" name="estado" type="text" />
+              <ErrorMessage component="span" name="estado" />
+              <hr />
+              <label htmlFor="complemento">Complemento: </label>
+              <Field id="complemento" name="complemento" type="text" />
+              <ErrorMessage component="span" name="complemento" />
+            </FormControl>
+            <SubmitButton>
+              <button
+                type="submit"
+                onClick={(event) => event.currentTarget.classList.toggle("btnSubmitClick")}
+              >
+                ENVIAR
+              </button>
+            </SubmitButton>
+          </Form>
+        </Formik>
+      </Container>
+    </div>
   );
 };
--- src/pages/cadastro/style.ts
+++ src/pages/cadastro/style.ts
@@ -1,19 +1,80 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const RobotoFont = css`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+`
 
 export const Container = styled.div`
+  ${RobotoFont}
+  label {
+    margin-bottom: 2px;
+    align-self: flex-start;
+  }
   input {
     border: 2px ridge gray;
+    font-size: 1fr;
     border-radius: 5px;
-    padding: 4px;
+    padding: 6px;
   }
+  span {
+    font-size: 10px;
+    background-color: darksalmon;
+    border-radius: 8px;
+    padding: 4px;
+    color: whitesmoke;
+    margin-top: 2px;
+    align-self: baseline;
+  }
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 20px;
+  background-color: whitesmoke;
+`;
+
+export const Title = styled.div`
+  font-size: 30px;
+  background-color: #610000;
+  padding: 20px;
+  color: white;
+`
+export const SharedControl = styled.div`
+  .sharedNome {
+    label {
+      
+    }
+    input {
+      margin-right: 8px;
+    }
+  }
+  .sharedAniversario {
+    input {
+      padding: 4px;
+    }
+  }
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 3px;
+`
+
+export const FormControl = styled.div`  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-bottom: 8px;
+`
+
+export const SubmitButton = styled.div`
   button {
-    border: 2px outset gray;
-    background-color: gray;
+    border: 2px outset #E8A323;
+    background-color: #E8A323; 
     border-radius: 5px;
     padding: 8px;
+    margin-top: 10px;
   }
-  button:hover {
-    border: 2px inset lightgray;
-    background-color: lightgray;    
+  .btnSubmitClick {
+    border: 2px inset #E6932B;
+    background-color: #E6932B;       
   }
-`;
+`
